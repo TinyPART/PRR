ea73c47a9307fefae4fff7b18d94be4eda200a07
Gregory Guche <gregory.guche@univ-lille.fr>
2025-01-27 14:20:50 +0100

    working riot

---
+++ RIOT/sys/shell/cmds/fmtbin.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     sys_shell_commands
+ * @{
+ *
+ * @file
+ * @brief       Shell command to format the file system
+ *
+ * @}
+ */
+
+#include "gnrc_xipfs.h"
+#include "shell.h"
+
+static int
+_fmtbin(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    tinyfs_format();
+
+    return 0;
+}
+
+SHELL_COMMAND(fmtbin, "format the file system", _fmtbin);
